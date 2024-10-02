a6daa4b7f7cd5dc822974d7ac261a06a89ba785d
Lahiru Nanayakkara <lahirunanayakkara924@gmail.com>
2024-10-02 23:05:10 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
+
+
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://mern-blog-backend-peach.vercel.app/",
+        target: import.meta.env.VITE_BACKEND_URL,
         secure: false,
       },
     },
